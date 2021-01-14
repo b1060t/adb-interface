689f76f1815fd466591b4a8f4cf6a2835cc3fec3
biobot <biobot94@gmail.com>
2021-01-14 17:19:02 +0800

    sync
    Signed-off-by: biobot <biobot94@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # adb-interface
 
 use Ninja to build
+
+```shell
+cmake -DCMAKE_TOOLCHAIN_FILE=${ndk}/build/cmake/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a -DANDROID_STL=c++_static -GNinja ..
+```
--- test.cpp
+++ test.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 
-int main()
-{
+#include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/string.hpp"
+
+int main(void) {
+#if 1
+	using namespace ftxui;
+
+	auto document =  //
+		vbox({
+			gauge(0.5)
+	});
+
+	document = document | size(WIDTH, LESS_THAN, 80);
+
+	auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+	Render(screen, document);
+
+	std::cout << screen.ToString() << std::endl;
+	//std::fstream file("1.txt", std::ios::out);
+	//file << screen.ToString();
+	//file.close();
+	std::cout << screen.PixelAt(0,1).character << std::endl;
+
+	return EXIT_SUCCESS;
+#else
 	std::cout << "aaa" << std::endl;
 	return 0;
+#endif
 }
